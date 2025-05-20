973befbf37d4f50a9da4460edb0bc3512900644e
365rafael <rafael.imu@gmail.com>
2025-05-20 11:57:28 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// Por enquanto, nenhum JS necessário.
+// Use este arquivo se quiser adicionar funcionalidades como tema escuro/claro, analytics, etc.
+
+console.log("Site carregado com sucesso!");
